bdc7d56920539644784eb12e57d02c00a5e976b6
Anna Tarasenko <annie.tarasenko@gmail.com>
2014-07-21 15:42:34 +0700

    Layout controllers have been extracted

--- src/main/java/org/happydev/lite/web/controller/EventBranchController.java
+++ src/main/java/org/happydev/lite/web/controller/EventBranchController.java
@@ -12,11 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -27,29 +23,6 @@ public class EventBranchController {
 
     @Autowired
     private ControllerUtils controllerUtils;
-
-    /**
-     * Shows a page for concrete Event branch or 404 Not found when ID is absent
-     * @param idStr a String value for ID
-     * @return ModelAndView object for this page
-     * @throws java.io.IOException Internal error of the servlet container
-     */
-    @RequestMapping(value = "/event-branch/{id}", method = RequestMethod.GET)
-    public ModelAndView eventBranchPage(
-            final HttpServletRequest request,
-            final HttpServletResponse response,
-            @PathVariable(value = "id") final String idStr
-    ) throws IOException {
-        ModelAndView modelAndView = new ModelAndView("event-branch-layout");
-        if (idStr == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            modelAndView = null;
-        } else {
-            modelAndView.addObject(idStr);
-        }
-
-        return modelAndView;
-    }
 
     /**
      * Returns a response object with Event branch object if possible.
--- src/main/java/org/happydev/lite/web/controller/MainLayoutController.java
+++ src/main/java/org/happydev/lite/web/controller/MainLayoutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class MainLayoutController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index() {
